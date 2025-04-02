68f50e02dee40a213d84dd1817bb7319733b4d30
CawaAkreadyTaken <daniele.cabassi01@gmail.com>
2025-04-02 17:25:39 +0200

    Maybe faster?

--- anonymizer/median_filtering.py
+++ anonymizer/median_filtering.py
@@ -1,75 +1,69 @@
-
+from utils.cross_correlation import crosscorr_2d, crosscorr_2d_color
+from utils.constants import OUTPUTPATH, BASEPATH, FINGERPRINTSPATH
+from utils.rotate_image import rotate_image, rotate_back_image
+from utils.pce import pce, pce_color
+import numpy as np
 import glob
 import cv2
-from utils.pce import pce, pce_color
-from utils.cross_correlation import crosscorr_2d, crosscorr_2d_color
-from utils.rotate_image import rotate_image, rotate_back_image
-from utils.constants import OUTPUTPATH, BASEPATH, FINGERPRINTSPATH
-import numpy as np
 import os
 
 PRINT = True
 
-def _print(stringa: str) -> None:
+def print_status(message: str) -> None:
     if PRINT:
-        print(stringa)
+        print(message)
 
-
-def median(img, kernel_size):
-    attacked = cv2.medianBlur(img, kernel_size) # it takes only one argument because it considers it squared
-    return attacked
+def median_filter(image, kernel_size: int):
+    # cv2.medianBlur only accepts a single kernel size (assumes square kernel)
+    return cv2.medianBlur(image, kernel_size)
 
 def main(devices: list[str]) -> None:
-
-    basepath = BASEPATH + 'D'
-    savepath = OUTPUTPATH + 'median_filtering/D'
-    fingerprint_path = FINGERPRINTSPATH + "Fingerprint_D"
+    base_path = os.path.join(BASEPATH, 'D')
+    save_path = os.path.join(OUTPUTPATH, 'median_filtering', 'D')
+    fingerprint_base = os.path.join(FINGERPRINTSPATH, "Fingerprint_D")
 
     for device in devices:
+        # Cache fingerprint for the device (load it once)
+        device_fingerprint_file = os.path.join(fingerprint_base, f"{device}.npy")
+        fingerprint = np.load(device_fingerprint_file)
+        fingerprint = np.repeat(fingerprint[..., np.newaxis], 3, axis=2)
 
-        files = sorted(glob.glob(basepath + device + '/nat/*.*'))
-        try:
-            os.mkdir(savepath+device)
-        except:
-            pass
-        
-        for img_name in files:
-                _print(img_name)
-                savepath_image = savepath + device + "/" + img_name[-18:]
-                # img = cv2.imread(img_name, cv2.IMREAD_GRAYSCALE).astype(np.float32)
-                img = cv2.imread(img_name)
-                img = rotate_image(img, img_name)
-                if img is None: 
-                    continue
-                device_fingerprint_path = fingerprint_path + device + ".npy" 
-                # print(device_fingerprint_path)
-                fingerprint = np.load(device_fingerprint_path)
-                fingerprint = np.repeat(fingerprint[..., np.newaxis], 3, axis=2)
+        # Create device save folder if it does not exist
+        device_save_path = os.path.join(save_path, device)
+        os.makedirs(device_save_path, exist_ok=True)
 
-                # median filtering
-                img_anonymized = median(img, 3)
+        files = sorted(glob.glob(os.path.join(base_path, device, 'nat', '*.*')))
+        for img_path in files:
+            print_status(img_path)
+            # Build the output image path; uses last 18 characters of original file name
+            save_path_image = os.path.join(device_save_path, os.path.basename(img_path)[-18:])
 
-                alpha_0 = 0
-                alpha_1 = 1
-                alpha_2 = 0
-                # v_pce = pce(crosscorr_2d(img, fingerprint)) # I dunno why it uses the real one
+            img = cv2.imread(img_path)
+            if img is None:
+                continue
+            img = rotate_image(img, img_path)
+            
+            # Apply median filtering
+            img_anonymized = median_filter(img, 3)
+            
+            # Initialize parameters for noise correction
+            alpha_0, alpha_1 = 0, 1
+            v_pce = pce_color(crosscorr_2d_color(img_anonymized, fingerprint))
+            print_status(v_pce)
+            iteration = 0
+            
+            # Loop until the condition is met or maximum iterations reached
+            while v_pce > 50 and iteration < 10:
+                alpha_2 = alpha_1 + (alpha_1 - alpha_0) / 10
+                alpha_0, alpha_1 = alpha_1, alpha_2
+                
+                # Vectorized noise correction over all channels
+                noise_residual = img - img_anonymized
+                img_anonymized = img - alpha_2 * noise_residual
+
                 v_pce = pce_color(crosscorr_2d_color(img_anonymized, fingerprint))
-                # print(img)
-                _print(v_pce)
-                iter = 0
-                while (v_pce>50 and iter<10):
-                    alpha_2 = alpha_1 + (alpha_1-alpha_0)/10
-                    alpha_0 = alpha_1
-                    alpha_1 = alpha_2
-                    for i in range(0,3):
-                        noise_residual = img[:,:,i] - img_anonymized[:,:,i]
-                        noise_residual_first = alpha_2* noise_residual
-                        img_anonymized[:,:,i] = img[:,:,i] - noise_residual_first
-                        # v_pce = pce(crosscorr_2d(img_anonymized, fingerprint))
-                    v_pce = pce_color(crosscorr_2d_color(img_anonymized, fingerprint))
-                    _print(v_pce)
-                    iter= iter +1
+                print_status(v_pce)
+                iteration += 1
 
-                img_anonymized = rotate_back_image(img_anonymized, img_name)
-                cv2.imwrite(savepath_image , img_anonymized)
-
+            img_anonymized = rotate_back_image(img_anonymized, img_path)
+            cv2.imwrite(save_path_image, img_anonymized)
