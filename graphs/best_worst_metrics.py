28ef6cb2cacaad8c1b57cccc6257bbcf5182acdc
achf01 <annachiarafortuna01@gmail.com>
2025-05-07 16:30:13 +0200

    findworstt for each metrics

--- graphs/best_worst_metrics.py
+++ graphs/best_worst_metrics.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 import glob
 import json 
+import sys
 import os
 
 
@@ -19,7 +20,12 @@ def parse_metrics(algorithms, chosen_devices):
         best_delta_ccn = 0.0
         best_delta_pce = 0.0
 
-        print("finding best result for ", algorithm_mapping.get(algorithm))
+        worst_wpsnr = sys.float_info.max
+        worst_ssim =  sys.float_info.max
+        worst_delta_ccn = sys.float_info.max
+        worst_delta_pce = sys.float_info.max
+
+        print("finding best and worst result for ", algorithm_mapping.get(algorithm))
 
         for device in range(1, 36):
             path= os.path.join(OUTPUTPATH, algorithm_mapping.get(algorithm), "D"+str(device).zfill(2))
@@ -32,12 +38,25 @@ def parse_metrics(algorithms, chosen_devices):
                         img_name = str(fname[-18:])
                         if metrics[img_name]["wpsnr"]>best_wpsnr:
                             best_wpsnr = metrics[img_name]["wpsnr"]
+                        if metrics[img_name]["wpsnr"]<worst_wpsnr:
+                            worst_wpsnr = metrics[img_name]["wpsnr"]
+
                         if metrics[img_name]["ssim"]>best_ssim:
                             best_ssim = metrics[img_name]["ssim"]
+                        if metrics[img_name]["ssim"]<worst_ssim:
+                            worst_ssim = metrics[img_name]["ssim"]
+                    
                         if metrics[img_name]["pce"]- metrics[img_name]["initial_pce"]>best_delta_pce:
                             best_delta_pce = metrics[img_name]["pce"]- metrics[img_name]["initial_pce"]
+                        if metrics[img_name]["pce"]- metrics[img_name]["initial_pce"]<worst_delta_pce:
+                            worst_delta_pce = metrics[img_name]["pce"]- metrics[img_name]["initial_pce"]
+
                         if metrics[img_name]["ccn"]- metrics[img_name]["initial_ccn"]>best_delta_ccn:
                             best_delta_ccn = metrics[img_name]["ccn"]- metrics[img_name]["initial_ccn"]
+                        if metrics[img_name]["ccn"]- metrics[img_name]["initial_ccn"]<worst_delta_ccn:
+                            worst_delta_ccn = metrics[img_name]["ccn"]- metrics[img_name]["initial_ccn"]
+
+    
                     
 
             except FileNotFoundError:
@@ -51,6 +70,9 @@ def parse_metrics(algorithms, chosen_devices):
         print("best ssim: ", best_ssim)
         print("best delta_pce: ", best_delta_pce)
         print("best delta_ccn: ", best_delta_ccn)
-        print("------------------------------------")
-
-
+        print("---")
+        print("worst wpsnr: ", worst_wpsnr)
+        print("worst ssim: ", worst_ssim)
+        print("worst delta_pce: ", worst_delta_pce)
+        print("worst delta_ccn: ", worst_delta_ccn)
+        print("----------------------------------")
