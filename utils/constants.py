efb10bcc0044ae8b9e6dbee4cb513a0b06094fb5
Enrico Carnelos <enrico.carnelos@unitn.it>
2025-05-12 14:38:12 +0200

    new SSD path

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,4 @@
-BASEPATH = '/media/SSD_mmlab/VISION_IMAGES/'
-OUTPUTPATH = '/media/SSD_mmlab/furious.transform/anonymized/'
+BASEPATH = '/media/SSD_new/VISION_IMAGES/'
+OUTPUTPATH = '/media/SSD_new/furious.transform/anonymized/'
 FINGERPRINTSPATH = 'fingerprints/'
 OUTPUT_GRAPHS_FOLDER = 'graphs/outputs/'
