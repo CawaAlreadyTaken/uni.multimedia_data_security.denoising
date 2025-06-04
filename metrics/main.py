3936d2563b9c24d2f03294ff3abde323f5bcd7de
CawaAkreadyTaken <daniele.cabassi01@gmail.com>
2025-06-04 16:30:54 +0200

    temp

--- metrics/main.py
+++ metrics/main.py
@@ -1,6 +1,6 @@
+from metrics.metrics_calculator import main as metrics_calculator
 from utils.parse_device_input import parse_device_input
 from utils.constants import OUTPUTPATH
-from metrics.metrics_calculator import main as metrics_calculator
 
 def menu():
     """
@@ -16,7 +16,8 @@ def menu():
         print("h) Show this menu description again")
         print("q) Quit anonymizer, go back")
 
-        choice = input("Select an option: ").strip().lower()
+        #choice = input("Select an option: ").strip().lower()
+        choice = '1'
 
         if choice == 'q':
             print("Exiting the program.")
@@ -33,13 +34,14 @@ def menu():
             continue
 
         # Now ask for the devices on which to apply the chosen algorithm(s).
-        devices_input = input("Enter the device number(s) between 1 and 35 to process.\n"
-                             "You can specify them in these ways:\n"
-                             " - Single device (e.g. '8')\n"
-                             " - Multiple comma-separated devices (e.g. '8,9,10')\n"
-                             " - A range with a dash (e.g. '6-10')\n"
-                             "Or any combination (e.g. '5,7,10-12').\n"
-                             "Your choice: ")
+        # devices_input = input("Enter the device number(s) between 1 and 35 to process.\n"
+        #                      "You can specify them in these ways:\n"
+        #                      " - Single device (e.g. '8')\n"
+        #                      " - Multiple comma-separated devices (e.g. '8,9,10')\n"
+        #                      " - A range with a dash (e.g. '6-10')\n"
+        #                      "Or any combination (e.g. '5,7,10-12').\n"
+        #                      "Your choice: ")
+        devices_input = '33'
 
         chosen_devices = parse_device_input(devices_input)
 
@@ -49,15 +51,15 @@ def menu():
 
         # Apply chosen algorithm(s)
         if choice == '1':
-            metrics_calculator(chosen_devices,OUTPUTPATH + 'fingerprint_removal' )
+            metrics_calculator(chosen_devices, OUTPUTPATH + 'fingerprint_removal')
         elif choice == '2':
-            metrics_calculator(chosen_devices,OUTPUTPATH + 'median_filtering' )
+            metrics_calculator(chosen_devices, OUTPUTPATH + 'median_filtering')
         elif choice == '3':
-            metrics_calculator(chosen_devices,OUTPUTPATH + 'adp2' )
+            metrics_calculator(chosen_devices, OUTPUTPATH + 'adp2')
         elif choice == 'all':
-            metrics_calculator(chosen_devices,OUTPUTPATH + 'fingerprint_removal' )
-            metrics_calculator(chosen_devices,OUTPUTPATH + 'median_filtering' )
-            metrics_calculator(chosen_devices,OUTPUTPATH + 'adp2' )
+            metrics_calculator(chosen_devices, OUTPUTPATH + 'fingerprint_removal')
+            metrics_calculator(chosen_devices, OUTPUTPATH + 'median_filtering')
+            metrics_calculator(chosen_devices, OUTPUTPATH + 'adp2')
 
         # Optionally, ask if the user wants to continue or break out
         cont = input("Do you want to process more? (y/n): ").strip().lower()
