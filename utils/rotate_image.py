57d4c1512fb479ed9f0b4335a589809a3e76c825
Enrico Carnelos <enrico.carnelos@unitn.it>
2025-06-06 10:09:43 +0200

    minor in rotate

--- utils/rotate_image.py
+++ utils/rotate_image.py
@@ -3,8 +3,9 @@ import exiftool
 
 def rotate_image(img, img_name):
     with exiftool.ExifTool() as et:
-        width, height = img.shape[1], img.shape[0]
+        
         if "EXIF:Orientation" not in et.get_metadata(img_name):
+            width, height = img.shape[1], img.shape[0]
             if width < height:
                 return cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
         orientation = et.get_metadata(img_name)["EXIF:Orientation"]
@@ -17,6 +18,7 @@ def rotate_image(img, img_name):
         img = cv2.rotate(img, cv2.ROTATE_180)
         
     # if orientation != 8 and orientation != 1 and orientation != 6 and orientation != 3:
+    width, height = img.shape[1], img.shape[0]
     if width < height:
         img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
     return img
