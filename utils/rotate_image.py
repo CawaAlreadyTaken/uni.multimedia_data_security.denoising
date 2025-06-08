c6c48749b832ed36b916c9b33d1c7d87221b06d2
CawaAkreadyTaken <daniele.cabassi01@gmail.com>
2025-06-08 10:03:37 +0200

    Updated rotate_image

--- utils/rotate_image.py
+++ utils/rotate_image.py
@@ -2,13 +2,11 @@ import cv2
 import exiftool
 
 def rotate_image(img, img_name):
-    with exiftool.ExifTool() as et:
-        
-        if "EXIF:Orientation" not in et.get_metadata(img_name):
-            width, height = img.shape[1], img.shape[0]
-            if width < height:
-                return cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-        orientation = et.get_metadata(img_name)["EXIF:Orientation"]
+    try:
+        with exiftool.ExifTool() as et:
+            orientation = et.get_metadata(img_name)["EXIF:Orientation"]
+    except KeyError:
+        return None
 
     if orientation == 8:
         img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
@@ -17,17 +15,18 @@ def rotate_image(img, img_name):
     if orientation == 3:
         img = cv2.rotate(img, cv2.ROTATE_180)
         
-    # if orientation != 8 and orientation != 1 and orientation != 6 and orientation != 3:
-    width, height = img.shape[1], img.shape[0]
-    if width < height:
-        img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    if orientation != 8 and orientation != 1 and orientation != 6 and orientation != 3:
+        return None
+
     return img
 
 def rotate_back_image(img, img_name):
-    with exiftool.ExifTool() as et:
-        if "EXIF:Orientation" not in et.get_metadata(img_name):
-            return None
-        orientation = et.get_metadata(img_name)["EXIF:Orientation"]
+    try:
+        with exiftool.ExifTool() as et:
+            orientation = et.get_metadata(img_name)["EXIF:Orientation"]
+    except KeyError:
+        return None
+
     if orientation == 8:
         img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
     if orientation == 6:
